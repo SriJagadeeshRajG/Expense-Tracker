153eed71e5cd15afae9eda9c14ee7d2cb8035591
Sri Jagadeesh Raj G <jagadeesh.23mis7268@vitapstudent.ac.in>
2026-06-30 18:41:52 +0530

    api URL updated for deployment

--- client/src/services/budgetApi.js
+++ client/src/services/budgetApi.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const budgetAPI = axios.create({
-  baseURL: "http://localhost:5000/api/budget",
+  baseURL: `${import.meta.env.VITE_API_URL}/budget`,
 });
 
 budgetAPI.interceptors.request.use(
