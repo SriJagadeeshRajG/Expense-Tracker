843c4830c1c958e306c93977ef1f381a90d70991
Sri Jagadeesh Raj G <jagadeesh.23mis7268@vitapstudent.ac.in>
2026-07-01 13:26:17 +0530

    debug logs new added

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -2,7 +2,6 @@ const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
   service: "gmail",
-
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
@@ -10,12 +9,26 @@ const transporter = nodemailer.createTransport({
 });
 
 const sendEmail = async (to, subject, html) => {
-  await transporter.sendMail({
-    from: `"Expense Tracker" <${process.env.EMAIL_USER}>`,
-    to,
-    subject,
-    html,
-  });
+  try {
+    console.log("Verifying transporter...");
+
+    await transporter.verify();
+
+    console.log("Transport verified.");
+
+    const info = await transporter.sendMail({
+      from: `"Expense Tracker" <${process.env.EMAIL_USER}>`,
+      to,
+      subject,
+      html,
+    });
+
+    console.log("Email sent:", info.messageId);
+
+  } catch (err) {
+    console.error("Email Error:", err);
+    throw err;
+  }
 };
 
 module.exports = sendEmail;
