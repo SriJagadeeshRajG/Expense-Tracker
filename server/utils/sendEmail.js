8a3a8c6ce05ac657fd9de0202fd6dc2fd6ae67a8
Sri Jagadeesh Raj G <jagadeesh.23mis7268@vitapstudent.ac.in>
2026-07-01 13:49:06 +0530

    fixing email issues

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -1,7 +1,9 @@
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
@@ -10,11 +12,7 @@ const transporter = nodemailer.createTransport({
 
 const sendEmail = async (to, subject, html) => {
   try {
-    console.log("Verifying transporter...");
-
-    await transporter.verify();
-
-    console.log("Transport verified.");
+    console.log("Sending email...");
 
     const info = await transporter.sendMail({
       from: `"Expense Tracker" <${process.env.EMAIL_USER}>`,
@@ -26,7 +24,7 @@ const sendEmail = async (to, subject, html) => {
     console.log("Email sent:", info.messageId);
 
   } catch (err) {
-    console.error("Email Error:", err);
+    console.error(err);
     throw err;
   }
 };
